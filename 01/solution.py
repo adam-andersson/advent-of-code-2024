b3426199082e41d27fa03cd9f988b9eb12e363e3
Adam Andersson <adama-98@hotmail.com>
2024-12-01 23:31:51 +0100

    1B: Naive solution

--- 01/solution.py
+++ 01/solution.py
@@ -9,4 +9,19 @@ res = 0
 for l_i, r_i in zip(l, r):
     res += abs(l_i - r_i)
 
-print(res)
+print("Part 1:", res)
+
+l_dict = {}
+for i in set(l):
+    l_dict[i] = l.count(i)
+
+r_dict = {}
+for i in set(r):
+    r_dict[i] = r.count(i)
+
+res2 = 0
+for k, v in l_dict.items():
+    if k in r_dict:
+        res2 += v * k * r_dict[k]
+
+print("Part 2:", res2)
