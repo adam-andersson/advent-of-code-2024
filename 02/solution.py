5faaa832c0ee22018218044a1f2be9f79e32ff3e
Adam Andersson <adama-98@hotmail.com>
2024-12-02 21:58:35 +0100

    refactor: use set operations for diffs

--- 02/solution.py
+++ 02/solution.py
@@ -4,15 +4,10 @@ data = sys.stdin.read().splitlines()
 
 
 def validate(r):
-    row_asc = sorted(r)
-    row_dec = sorted(r, reverse=True)
-
-    if r == row_asc or r == row_dec:
-        for i in range(1, len(row_asc)):
-            diff = row_asc[i] - row_asc[i - 1]
-            if not diff in {1, 2, 3}:
-                return False
-        return True
+    diffs = set([r[i] - r[i - 1] for i in range(1, len(r))])
+    if not (diffs.issubset({1, 2, 3}) or diffs.issubset({-1, -2, -3})):
+        return False
+    return True
 
 
 res1 = 0
