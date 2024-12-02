146e203a9012a848b4f5a82f077329153dd8d9f6
Adam Andersson <adama-98@hotmail.com>
2024-12-02 21:43:49 +0100

    2B: Naive solution

--- 02/solution.py
+++ 02/solution.py
@@ -2,26 +2,29 @@ import sys
 
 data = sys.stdin.read().splitlines()
 
-res1 = 0
-for line in data:
-    row = [int(n) for n in line.split(" ")]
-    row_asc = sorted(row)
-    row_dec = sorted(row, reverse=True)
 
-    if row == row_asc or row == row_dec:
-        should_add = True
+def validate(r):
+    row_asc = sorted(r)
+    row_dec = sorted(r, reverse=True)
+
+    if r == row_asc or r == row_dec:
         for i in range(1, len(row_asc)):
             diff = row_asc[i] - row_asc[i - 1]
             if diff != 1 and diff != 2 and diff != 3:
-                should_add = False
-                break
-        if should_add:
-            res1 += 1
+                return False
+        return True
 
+
+res1 = 0
+for line in data:
+    row = [int(n) for n in line.split(" ")]
+    res1 += 1 if validate(row) else 0
 
 print("Part 1:", res1)
 
-
 res2 = 0
+for line in data:
+    row = [int(n) for n in line.split(" ")]
+    res2 += 1 if any([validate(row[:i] + row[i + 1 :]) for i in range(len(row))]) else 0
 
-print("Part 2:", res2)
+print("Part 2:", res1)
