b91513d8089f7cce19cf02f70913a359e5e57e7b
Adam Andersson <adama-98@hotmail.com>
2024-12-06 13:14:51 +0100

    5B: Bubble sort by rules

--- 05/solution.py
+++ 05/solution.py
@@ -33,4 +33,43 @@ for serie in series:
     if all([validate(serie, filtered_rules, n) for n in serie]):
         res1 += serie[int(len(serie) / 2)]
 
-print(res1)
+print("Part 1:", res1)
+
+res2 = 0
+
+nums_before_dict = {}
+nums_in_series = set()
+for a, b in rules:
+    nums_in_series.add(a)
+    nums_in_series.add(b)
+    if a not in nums_before_dict:
+        nums_before_dict[a] = set()
+    nums_before_dict[a].add(b)
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for _ in range(n):
+        swapped = False
+        for i in range(n - 1):
+            left, right = arr[i], arr[i + 1]
+            if right in nums_before_dict and left in nums_before_dict[right]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+for serie in series:
+    filtered_rules = [r for r in rules if (r[0] in serie and r[1] in serie)]
+
+    if all([validate(serie, filtered_rules, n) for n in serie]):
+        continue
+
+    sorted_serie = bubble_sort(list(serie))
+
+    res2 += sorted_serie[int(len(sorted_serie) / 2)]
+
+
+print("Part 2:", res2)
