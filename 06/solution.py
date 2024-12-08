a7c1986acb1d378d5f071d1bcdd313f208efe93c
Adam Andersson <adama-98@hotmail.com>
2024-12-09 00:32:00 +0100

    6B: Brute force maniac

--- 06/solution.py
+++ 06/solution.py
@@ -1,15 +1,24 @@
 import sys
 
+DIRECTIONS = {
+    "N": (-1, 0),
+    "S": (1, 0),
+    "W": (0, -1),
+    "E": (0, 1),
+}
+
+START_DIRECTION = DIRECTIONS["N"]
+
 
 def rotate90(dir):
-    if dir == directions["N"]:
-        return directions["E"]
-    if dir == directions["E"]:
-        return directions["S"]
-    if dir == directions["S"]:
-        return directions["W"]
-    if dir == directions["W"]:
-        return directions["N"]
+    if dir == DIRECTIONS["N"]:
+        return DIRECTIONS["E"]
+    if dir == DIRECTIONS["E"]:
+        return DIRECTIONS["S"]
+    if dir == DIRECTIONS["S"]:
+        return DIRECTIONS["W"]
+    if dir == DIRECTIONS["W"]:
+        return DIRECTIONS["N"]
 
 
 def find_start(g):
@@ -19,28 +28,36 @@ def find_start(g):
                 return (r, c)
 
 
-directions = {
-    "N": (-1, 0),
-    "S": (1, 0),
-    "W": (0, -1),
-    "E": (0, 1),
-}
+def grid_walk(g, pos, dir, recurse=False, rec_pos=(0, 0)):
+    seen = {}
+    i, j = pos
+    while 0 <= i < len(g) and 0 <= j < len(g[0]):
+        current = g[i][j]
+        if current == "#":
+            i, j = i - dir[0], j - dir[1]
+            dir = rotate90(dir)
+        else:
+            if not (i, j) in seen:
+                seen[(i, j)] = set()
+            elif dir in seen[(i, j)]:
+                obstacles.add(rec_pos)
+                return
+            seen[(i, j)].add(dir)
+
+            if recurse:
+                g_copy = [row[:] for row in g]
+                g_copy[i][j] = "#"
+                grid_walk(g_copy, pos, DIRECTIONS["N"], False, (i, j))
+
+        i, j = i + dir[0], j + dir[1]
+    return len(seen)
+
 
 grid = [list(x.strip()) for x in sys.stdin.readlines()]
+obstacles = set()
 
-
-i, j = find_start(grid)
-direction = directions["N"]
-
-while 0 <= i < len(grid) and 0 <= j < len(grid[0]):
-    current = grid[i][j]
-    if current == "#":
-        i, j = i - direction[0], j - direction[1]
-        direction = rotate90(direction)
-    else:
-        grid[i][j] = "X"
-    i, j = i + direction[0], j + direction[1]
-
-res1 = sum([r.count("X") for r in grid])
+res1 = grid_walk(grid, find_start(grid), START_DIRECTION)
+_res2 = grid_walk(grid, find_start(grid), START_DIRECTION, True)
 
 print("Part 1:", res1)
+print("Part 2:", len(obstacles))
