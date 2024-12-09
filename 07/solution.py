e91214d1d02aba03383fc56eee7c6b15ec869ae6
Adam Andersson <adama-98@hotmail.com>
2024-12-09 01:07:46 +0100

    7B: Add third case ||

--- 07/solution.py
+++ 07/solution.py
@@ -5,9 +5,9 @@ data = [[l.strip() for l in line.split(":")] for line in sys.stdin.read().splitl
 parsed_data = [(int(l[0]), list(map(int, l[1].split(" ")))) for l in data]
 
 
-def run_equation(op, eq, acc, target):
+def run_equation(op, eq, acc, target, part2=False):
     eq = eq[:]
-    if len(eq) == 0:
+    if len(eq) == 0 or acc > target:
         if acc == target:
             return True
         return False
@@ -18,11 +18,19 @@ def run_equation(op, eq, acc, target):
         acc += value
     elif op == "*":
         acc *= value
+    elif op == "||":
+        acc = int(str(acc) + str(value))
 
-    return any([run_equation("+", eq, acc, target), run_equation("*", eq, acc, target)])
+    return any(
+        [
+            run_equation("+", eq, acc, target, part2),
+            run_equation("*", eq, acc, target, part2),
+            part2 & run_equation("||", eq, acc, target, part2),
+        ]
+    )
 
 
-res1 = 0
+res1, res2 = 0, 0
 for looking_for, equation in parsed_data:
     if any(
         [
@@ -31,8 +39,15 @@ for looking_for, equation in parsed_data:
         ]
     ):
         res1 += looking_for
-
-res2 = None
+        res2 += looking_for
+    elif any(
+        [
+            run_equation("+", equation, 0, looking_for, True),
+            run_equation("*", equation, 0, looking_for, True),
+            run_equation("||", equation, 0, looking_for, True),
+        ]
+    ):
+        res2 += looking_for
 
 print("Part 1:", res1)
 print("Part 2:", res2)
