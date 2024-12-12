745a1c435332c77a4a853b5a5b5ff6503f8b134f
Adam Andersson <adama-98@hotmail.com>
2024-12-12 22:32:29 +0100

    11B: Fix cache key and do 75 depth

--- 11/solution.py
+++ 11/solution.py
@@ -5,27 +5,26 @@ data = list(sys.stdin.read().strip().split(" "))
 dp = {}
 
 
-def stone_blink(stone, blink=0):
-    stone_key = stone + "/" + str(blink)
-    if blink == MAX_BLINK:
-        return stone
+def stone_blink(stone, blink):
+    if blink <= 0:
+        return 1
 
-    if stone in dp:
+    stone_key = (stone, blink)
+
+    if stone_key in dp:
         return dp[stone_key]
 
     if stone == "0":
-        dp[stone_key] = stone_blink("1", blink + 1)
+        dp[stone_key] = stone_blink("1", blink - 1)
     elif len(stone) % 2 == 0:
         mid = len(stone) // 2
-        dp[stone_key] = (
-            stone_blink(str(int(stone[:mid])), blink + 1)
-            + " "
-            + stone_blink(str(int(stone[mid:])), blink + 1)
+        dp[stone_key] = stone_blink(str(int(stone[:mid])), blink - 1) + stone_blink(
+            str(int(stone[mid:])), blink - 1
         )
     else:
-        dp[stone_key] = stone_blink(str(2024 * int(stone)), blink + 1)
+        dp[stone_key] = stone_blink(str(2024 * int(stone)), blink - 1)
     return dp[stone_key]
 
 
-MAX_BLINK = 25
-print(sum(len(stone_blink(s).split()) for s in data))
+for blinks, part in zip((25, 75), (1, 2)):
+    print(f"Part {part}:", sum([stone_blink(s, blinks) for s in data]))
